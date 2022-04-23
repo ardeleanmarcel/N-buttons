118183e1bbe9c6c7de0e8d9ebf7e6fabce92ba74
Marcel Ardelean <102534901+ardeleanmarcel@users.noreply.github.com>
2022-04-23 12:57:19 +0300

    Update n-buttons.js

--- n-buttons.js
+++ n-buttons.js
@@ -2,20 +2,20 @@ function addNumber() {
 	let numberAdded = document.getElementById("nrAdded").value;
 	const random = Math.floor(Math.random() * (numberAdded - 1 + 1)) + 1;
 	while (numberAdded > 0) {
-    let btn = document.createElement("button");
-    let buttonNumber = numberAdded;
-    btn.innerHTML = "ClickMe";
-    btn.type = "button";
-    btn.onclick = function() {
-      if (random == buttonNumber + 1) {
-        alert("You WIN! Congratulations!");
-      } else {
-        alert("You LOSE! Try again!")
-      }
-      location.reload();
-    };
-    document.body.appendChild(btn);
-    --numberAdded;
-    --buttonNumber;
+		let btn = document.createElement("button");
+		let buttonNumber = numberAdded;
+		btn.innerHTML = "ClickMe";
+		btn.type = "button";
+		btn.onclick = function() {
+			if (random == buttonNumber + 1) {
+				alert("You WIN! Congratulations!");
+			} else {
+				alert("You LOSE! Try again!")
+			}
+			location.reload();
+		};
+		document.body.appendChild(btn);
+		--numberAdded;
+		--buttonNumber;
 	}
 }
